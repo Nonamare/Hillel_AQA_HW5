80126da09db60a4dc2099920a00d39395d83012d
Nonamare <81253473+Nonamare@users.noreply.github.com>
2023-05-15 20:19:08 +0300

    Update User.java

--- HomeWork5/src/ua/hillel/homework5/User.java
+++ HomeWork5/src/ua/hillel/homework5/User.java
@@ -5,7 +5,7 @@ public class User {
     // повинні мати модифікатор доступу private.
     // Зробити геттери і сеттери з валідацією для private полів(валідація довільна).
 
-    // I know I violates code convention using caps lock - it's for special purpose
+    // I know I violate code convention using caps lock - it's for special purpose
     private static boolean DELETE_ME;
 
     private static final String defaultPassword = "123456";
